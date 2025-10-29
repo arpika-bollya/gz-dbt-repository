e82d3a1029ae494d7d4e0df3c3e774bd5dcd2640
bollya.arpika@gmail.com <bollya.arpika@gmail.com>
2025-10-29 16:34:54 +0000

    Generate One Model per Source

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        CAST(ship_cost AS FLOAT64) as ship_cost,
+        orders_id,
+        shipping_fee,
+        logcost,
+
+    from source
+
+)
+
+select * from renamed
